96818c2d25bd00b88d7d1827203cf8ecc77dc13a
David Sveningsson <ext@sidvind.com>
2017-07-15 17:45:31 +0200

    creep steal

--- game/items/creep.ts
+++ game/items/creep.ts
@@ -1,5 +1,6 @@
 import { Item } from 'item';
 import { IEntityProperty } from 'entity'; // eslint-disable-line no-unused-vars
+import { Texture } from 'texture';
 
 const defaults: IEntityProperty = {
 	value: 1,
@@ -7,10 +8,33 @@ const defaults: IEntityProperty = {
 
 export class Creep extends Item {
 	value: number;
+	stolen: boolean;
+	otherDiffuse: Texture;
 
 	constructor(gl: WebGL2RenderingContext, options?: IEntityProperty){
 		options = Object.assign({}, defaults, options);
 		super(gl, options);
 		this.value = options.value;
+		this.stolen = false;
+		this.otherDiffuse = null;
+
+		Texture.load(gl, '/textures/beaver-theft.png').then((texture: Texture) => {
+			this.otherDiffuse = texture;
+		}).catch((fallback: Texture) => {
+			this.otherDiffuse = fallback;
+		});
+	}
+
+	steal(){
+		this.stolen = true;
+		this.diffuse = this.otherDiffuse;
+	}
+
+	update(dt: number){
+		if (!this.stolen && this.position.elements[1] > -4){
+			this.steal();
+		}
+
+		super.update(dt);
 	}
 }
